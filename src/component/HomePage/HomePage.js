8ad6fb169ce2e0493512caa41f20e563f1ddb257
sharifok1 <sharifok1@gmail.com>
2022-01-09 00:08:20 +0600

    update homepage ui

--- src/component/HomePage/HomePage.js
+++ src/component/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import Button from '@restart/ui/esm/Button';
 import React, { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Hom4Service from '../Hom4service/Hom4Service';
 import './HomePage.css'
@@ -25,7 +25,18 @@ const HomePage = () => {
                 )    
             }
             </Row>
-           <Link to ="/Services"><Button className="see-more">See more</Button></Link>
+           <Link to ="/Services"><Button className="see-more p-2 m-4">See more</Button></Link>
+           <div>
+               <Row xs={1} md={2}> 
+                   <Col>
+                         <img className='img-fluid' src="https://media-eng.dhakatribune.com/uploads/2019/12/p1-31-12-19-motijheel-ideal-school-and-college-psc-and-jsc-result-2-1577813156866.jpg" alt="" />
+                   </Col>
+                   <Col>
+                         <img className='img-fluid' src="https://www.laprensalatina.com/wp-content/uploads/2021/09/17874640w.jpg" alt="" />
+                   </Col>
+               </Row>
+               
+           </div>
         </div>
     );
 };
